30a932f33f5ea8378feb5b50291e81da26f24082
Leon Kloker <leonkl@stanford.edu>
2023-05-10 22:14:42 +0000

    simulation works

--- simulation.py
+++ simulation.py
@@ -10,84 +10,97 @@ def homogeneous_poisson_times(t, T, lambda0):
         raise ValueError("t must be less than or equal to T")
     nevents = np.random.poisson(lambda0 * (T - t))
     times = np.random.uniform(t, T, nevents)
-    return np.sort(times)
+    return sorted(times)
 
-# thinning to generate inhomogeneous poisson process
-def thinning(events, ratio):
-    return events[np.random.uniform(size=events.shape[0]) < ratio]
-
-def lambdap(lambda0p, alpha, beta, eventtimes, t):
-    if eventtimes.size == 0:
-        return lambda0p
+# calculate lambda(t) for exponential hawkes process
+def lambdap(lambda_0, alpha, beta, past_events, t):
+    past_events = np.array(past_events)
+    if np.sum(t < past_events) > 0:
+        raise ValueError("t must be greater than all past event times")
     
-    if not np.isscalar(t):
-        t = t.reshape(-1,1)
-        eventtimes = eventtimes.reshape(1,-1)
-    
-    if np.sum(t - eventtimes < 0) > 0:
-        raise ValueError("t must be greater than all event times")
-    
-    if not np.isscalar(t):
-        return lambda0p + alpha * beta * np.sum(np.exp(-beta * (t - eventtimes)), axis=1)
-    return lambda0p + alpha * beta * np.sum(np.exp(-beta * (t - eventtimes)))
+    return lambda_0 + alpha * beta * np.sum(np.exp(-beta * (t - past_events)))
     
 
 # define baseline intensities parameters
-lambda0p = 0.3
-lambda0n = 1
+lambda_0_p = 0.3
+lambda_0_n = 1
 
 # define exponential hawkes kernel parameters
-alpha = 0.5
+alpha = 1.001
 beta = 1
 
 # start time and end time
 t = 0
-T = 400
+T = 100
 
 # initial spread
-S = [3]
+S = [20]
 
 # initialize list of event times
 tau = [0]
 
 # initialize list of spread-opening event times
-eventtimesp = np.array([])
+times_p = []
 
 # generate homogeneous event times for spread-closing events
-eventtimesn = homogeneous_poisson_times(0, T, lambda0n)
+times_n = homogeneous_poisson_times(0, T, lambda_0_n)
 
-while True:
-    # calculate lambda^plus_max
-    if eventtimesp.size == 0:
-        lambdap_max = lambda0p
-    else:
-        lambdap_max = lambdap(lambda0p, alpha, beta, eventtimesp, eventtimesp[-1])
+# maximal lambda_p
+lambda_p_max = lambda_0_p
 
-    # generate homogeneous event times for spread opening events
-    eventtimesp_new = homogeneous_poisson_times(t, T, lambdap_max)
-
-    # thin event times
-    lambdap_t = lambdap(lambda0p, alpha, beta, eventtimesp, eventtimesp_new)
-    eventimesp_new = thinning(eventtimesp_new, lambdap_t / lambdap_max)
-
-    # check if no more events
-    if eventimesp_new.size == 0 or eventimesp_new[0] >= T:
-        break
-
-    # there is another spread opening event
-    else:
-        t = eventtimesp_new[0]
-        eventtimesp = np.append(eventtimesp, t)
+# generate spread opening events independent of spread closing events
+while t < T:
     
-    while S[-1] > 1 and eventtimesn.size > 0 and eventtimesn[0] <= t:
+    # generate homogeneous event times for spread opening events
+    times_p_new = homogeneous_poisson_times(t, T, lambda_p_max)
+
+    newmax = False
+
+    while not newmax:
+
+        # check if there are any events
+        if len(times_p_new) == 0:
+            t = T
+            break
+
+        # take the first event time t
+        t = times_p_new.pop(0)
+
+        # calculate lambda(t)
+        lambda_t = lambdap(lambda_0_p, alpha, beta, times_p, t)
+
+        # check if event occurs
+        if np.random.uniform(0, 1) < lambda_t / lambda_p_max:
+            times_p.append(t)
+
+            # check if lambda(t) is new max due to this event
+            if lambda_t + alpha*beta > lambda_p_max:
+                lambda_p_max = lambda_t + alpha*beta
+                newmax = True
+
+# merge spread opening and closing events
+while len(times_n) > 0:
+
+    # add opening events until next closing event
+    while len(times_p) > 0 and times_p[0] < times_n[0]:
+        tau.append(times_p.pop(0))
+        S.append(S[-1] + 1)
+
+    # add closing event only if spread is open
+    if S[-1] > 1:
+        tau.append(times_n.pop(0))
         S.append(S[-1] - 1)
-        tau.append(eventtimesn[0])
-        eventtimesn = np.delete(eventtimesn, 0)
 
+    # remove the closing event if spread is closed
+    else:
+        times_n.pop(0)
+
+# add remaining opening events
+while len(times_p) > 0:
+    tau.append(times_p.pop(0))
     S.append(S[-1] + 1)
-    tau.append(t)
 
-name = "spread_lambda_{}_{}_alpha_{}_beta_{}_T_{}.png".format(lambda0p, lambda0n, alpha, beta, T)
+name = "spread_lambda_{}_{}_alpha_{}_beta_{}_T_{}.png".format(lambda_0_p, lambda_0_n, alpha, beta, T)
 plt.figure()
 plt.step(tau, S, label="Spread")
 plt.xlabel("Time")
